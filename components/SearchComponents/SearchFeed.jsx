0579e0c420bb47acc9cce361938e50ee3994232d
dsoumitra693 <dsoumitra693@gmail.com>
2023-09-17 13:45:31 +0530

    adding lottie files

--- components/SearchComponents/SearchFeed.jsx
+++ components/SearchComponents/SearchFeed.jsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, FlatList, Keyboard, View, } from 'react-native'
 import React from 'react'
 import MovieCard from '../MovieCard'
 import Icon from 'react-native-vector-icons/Feather'
+import LottieView from 'lottie-react-native';
 import { normalize } from '../../fontsNormalisation';
 
 
